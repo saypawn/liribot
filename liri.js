2132d82d7bcddcbd52aa2cd4294c6ae64106a63f
Jeffery Saephan <jeffery.saephan@gmail.com>
2019-12-03 19:40:56 -0800

    added movie search fx

--- liri.js
+++ liri.js
@@ -78,4 +78,53 @@ var getMyBands = function(artist) {
       console.log(`pcoming concerts for${artist}:`);
       for (var i = 0; i < jsonData.length; i++) {
         var show = jsonData[i];
+        // Print data about each concert
+        // If a concert doesn't have a region, display the country instead
+        // Use moment to format the date
+        console.log(
+          show.venue.city +
+            "," +
+            (show.venue.region || show.venue.country) +
+            " at " +
+            show.venue.name +
+            " " +
+            moment(show.datetime).format("MM/DD/YYYY")
+        );
+
+      }
+
+    }
+
+  );
+
+};
+
+
+
+// Function for running a Movie Search
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
+
+  var urlHit =
+    `http://www.omdbapi.com/?t=${movieName}&y=&plot=full&tomatoes=true&apikey=trilogy`;
+  axios.get(urlHit).then(
+    function(response) {
+      var jsonData = response.data;
+      console.log(`Title:${jsonData.Title}`);
+      console.log(`Year:${jsonData.Year}`);
+      console.log(`Rated:${jsonData.Rated}`);
+      console.log(`IMDBRating:${jsonData.imdbRating}`);
+      console.log(`Country:${jsonData.Country}`);
+      console.log(`Language:${jsonData.Language}`);
+      console.log(`Plot:${jsonData.Plot}`);
+      console.log(`Actors:${jsonData.Actors}`);
+      console.log(`Rotten Tomatoes Rating:${jsonData.Ratings[1].Value}`);
+
+    }
+
+  );
+
+};
 
